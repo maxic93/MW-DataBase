06c601bc2fdcc65bdb2dc03d59961ac37804bc39
Maxi <maxicabral36@gmail.com>
2023-07-12 17:17:03 -0300

    DB de los monstruos

---
+++ dataMW.js
@@ -0,0 +1,49 @@
+
+const Vadamagma = {
+    id: 1,
+    img: "https://i.postimg.cc/vTJyyzQy/pngegg.png",
+    Ataque: 84,
+    Defensa: 78,
+    HP: 78,
+    Tipo: "fire",
+    Velocidad: 100,
+    name: "Vadamagma",
+    clase: "Legendario"
+}
+
+const SeñorDeLosMares = {
+    id: 2,
+    img: "https://i.postimg.cc/7hn82T08/pngegg-4.png",
+    Ataque: 83,
+    Defensa: 100,
+    HP: 79,
+    Tipo: "water",
+    Velocidad: 78,
+    name: "Señor de los Mares",
+    clase: "Legendario"
+}
+
+const Gea = {
+    id: 3,
+    img: "https://i.postimg.cc/ZnJZVh8d/pngegg-3.png",
+    Ataque: 82,
+    Defensa: 83,
+    HP: 80,
+    Tipo: "grass",
+    Velocidad: 80,
+    name: "Gea",
+    clase: "Legendario"
+}
+
+const Iceboss = {
+    id: 4,
+    img: "https://i.postimg.cc/ZR3XGDbx/pngegg-1.png",
+    Ataque: 85,
+    Defensa: 100,
+    HP: 90,
+    Tipo: "ice",
+    Velocidad: 85,
+    name: "Iceboss",
+    clase: "Legendario"
+}
+
